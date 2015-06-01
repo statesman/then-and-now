bffed92f6f45bf94c2b3c2da76164a9e9cf96b07
Christian McDonald <cmcdonald@statesman.com>
2015-06-01 17:10:04 -0500

    archifying this based on lake travis. Allows use of .aml file for content.

--- src/js/main.js
+++ src/js/main.js
@@ -1,13 +1,24 @@
 (function($) {
 
-  "use strict";
+  'use strict';
 
-    //twentytwenty sliders for index
-    $(window).load(function() {
-      $("#thenAndNow1").twentytwenty();
-     });
-    $(window).load(function() {
-      $("#thenAndNow2").twentytwenty();
+
+  $(function() {
+    $('div.then-and-now').each(function(i, el) {
+      var $slider = $(this);
+
+      $slider.imagesLoaded()
+        .done(function() {
+          $slider.twentytwenty();
+          var images = $slider.children('img');
+          $slider.find('.twentytwenty-before-label').attr('data-content', $(images[0]).attr('data-date'));
+          $slider.find('.twentytwenty-after-label').attr('data-content', $(images[1]).attr('data-date'));
+        })
+        .fail(function() {
+          $slider.parent().remove();
+        });
+
     });
+  });
 
 }(jQuery));
