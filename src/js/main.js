f6de9a4a35787d98e03673f57a88d4e8b79a05c0
Christian McDonald <cmcdonald@statesman.com>
2015-06-03 14:30:08 -0500

    removing !== false check as it breaks default labels. Closes #9. Will address again if we see future problems in edge browsers

--- src/js/main.js
+++ src/js/main.js
@@ -11,12 +11,12 @@
         .done(function() {
           $slider.twentytwenty();
           var images = $slider.children('img');
-          if (typeof $(images[0]).attr('data-label') !== 'undefined' || $(images[0]).attr('data-label') !== false) {
+          if (typeof $(images[0]).attr('data-label') !== 'undefined') {
             $slider.find('.twentytwenty-before-label').attr('data-content', $(images[0]).attr('data-label'));
           } else {
             $slider.find('.twentytwenty-before-label').attr('data-content', 'Then');
           }
-          if (typeof $(images[1]).attr('data-label') !== 'undefined' || $(images[1]).attr('data-label') !== false) {
+          if (typeof $(images[1]).attr('data-label') !== 'undefined') {
             $slider.find('.twentytwenty-after-label').attr('data-content', $(images[1]).attr('data-label'));
           } else {
             $slider.find('.twentytwenty-after-label').attr('data-content', 'Now');
